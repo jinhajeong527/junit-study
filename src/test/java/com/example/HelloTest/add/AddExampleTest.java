d9180377f8c0497fd3ad3f5253f2282a2994f423
jinhajeong <jinha4ever@gmail.com>
2022-12-04 06:44:04 +0900

    배열이 비어있을 때 및 하나만 있을 때도 테스트 작성

--- src/test/java/com/example/HelloTest/add/AddExampleTest.java
+++ src/test/java/com/example/HelloTest/add/AddExampleTest.java
@@ -3,7 +3,7 @@ package com.example.HelloTest.add;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
-public class addExampleTest {
+public class AddExampleTest {
 
     @Test
     public void addMethodTest() {
@@ -19,6 +19,22 @@ public class addExampleTest {
         AddExample addExample = new AddExample();
         int actualResult = addExample.calculationSum(new int[] {1, 2, 3});
         int expectedResult = 6;
+        assertEquals(expectedResult, actualResult);
+    }
+
+    //Calculate with empty single
+    @Test
+    public void calculateSumTestEmpty() {
+        AddExample addExample = new AddExample();
+        int actualResult = addExample.calculationSum(new int[] {});
+        int expectedResult = 0; // 빈 배열을 넘겼기 때문에
+        assertEquals(expectedResult, actualResult);
+    }
+    @Test
+    public void calculateSumTestOne() {
+        AddExample addExample = new AddExample();
+        int actualResult = addExample.calculationSum(new int[] {1});
+        int expectedResult = 1;
         assertEquals(expectedResult, actualResult);
     }
 }
